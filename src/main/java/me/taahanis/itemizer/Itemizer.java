8087d1f728551cbe6d604efbdceba9f7c6d73240
ThePhoenixHub <spacerocket62@gmail.com>
2019-09-30 19:30:21 -0700

    i really forgot to add to the enable and disable functions lmao

--- src/main/java/me/taahanis/itemizer/Itemizer.java
+++ src/main/java/me/taahanis/itemizer/Itemizer.java
@@ -7,13 +7,16 @@ public class Itemizer extends JavaPlugin {
     @Override
     public void onEnable()
     {
+        getLogger().info("§6[§bItemizer§6] §aEnabling ITEMIZER v" + getDescription().getVersion());
+        getLogger().info("§6[§bItemizer§6] §aCreated by: " + getDescription().getAuthors().get(0));
         getCommand("itemizer").setExecutor(new ItemizerCommand());
     }
 
     @Override
     public void onDisable()
     {
-
+        getLogger().info("§6[§bItemizer§6] §aDisabling ITEMIZER v" + getDescription().getVersion());
+        getLogger().info("§6[§bItemizer§6] §aCreated by: " + getDescription().getAuthors().get(0));
     }
 
 
