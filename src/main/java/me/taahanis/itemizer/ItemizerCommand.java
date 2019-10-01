65ed4492f3933e9655a3e46768e560889056409b
ThePhoenixHub <spacerocket62@gmail.com>
2019-09-30 19:26:35 -0700

    ig it's done

--- src/main/java/me/taahanis/itemizer/ItemizerCommand.java
+++ src/main/java/me/taahanis/itemizer/ItemizerCommand.java
@@ -23,6 +23,8 @@ public class ItemizerCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
     {
 
+        // naming done, title done, lores done, clearing all done, heads done,
+
         if (args.length <= 0)
         {
             sender.sendMessage("§c===§6[§bItemizer§6] §aCommands§c===");
@@ -30,7 +32,9 @@ public class ItemizerCommand implements CommandExecutor {
             sender.sendMessage("");
             sender.sendMessage("   §b/itemizer title <title> - §cSet the title of your book (Must be signed)");
             sender.sendMessage("");
-            sender.sendMessage("   §b/itemizer lore <set | clear> [lore] - §cSet or clear the lore of your item.");
+            sender.sendMessage("   §b/itemizer author <author> - §cSets the title of your book (Must be signed)");
+            sender.sendMessage("");
+            sender.sendMessage("   §b/itemizer lore help - §cProvides the options for the lore subcommand.");
             sender.sendMessage("");
             sender.sendMessage("   §b/itemizer clearall - §cClears all item meta of your held item.");
             sender.sendMessage("");
@@ -53,6 +57,7 @@ public class ItemizerCommand implements CommandExecutor {
 
         ItemStack item = player.getInventory().getItemInMainHand();
         ItemMeta itemMeta = item.getItemMeta();
+
 
         if (args[0].equalsIgnoreCase("name"))
         {
@@ -89,7 +94,8 @@ public class ItemizerCommand implements CommandExecutor {
             }
 
 
-            BookMeta meta = (BookMeta) item.getItemMeta();
+            BookMeta meta = (BookMeta) itemMeta;
+            assert meta != null;
             meta.setTitle(ChatColor.translateAlternateColorCodes('&', title));
 
 
@@ -99,14 +105,51 @@ public class ItemizerCommand implements CommandExecutor {
             return true;
         }
 
+        if (args[0].equalsIgnoreCase("author"))
+        {
+
+
+            if (item.getType() != Material.WRITTEN_BOOK)
+            {
+                player.sendMessage("§6[§bItemizer§6] §aYou are not holding a book in your hand. \nIf you are holding a book, it must be written.");
+                return true;
+            }
+            if (args.length < 2)// /argslength0 author1 name2 /args args0 args1 args2etcetc
+            {
+                player.sendMessage("§6[§bItemizer§6] §aYou did not specify an author.");
+                return true;
+            }
+            String authorName = StringUtils.join(ArrayUtils.subarray(args, 1, args.length), " ");
+
+            BookMeta meta = (BookMeta) itemMeta;
+            assert meta != null;
+            meta.setAuthor(ChatColor.translateAlternateColorCodes('&', authorName));
+            item.setItemMeta(meta);
+            player.sendMessage("§6[§bItemizer§6] §aYour book's author has been set to '" + ChatColor.translateAlternateColorCodes('&', authorName) + "'");
+            return true;
+        }
+
         if (args[0].equalsIgnoreCase("lore"))
         {
             if (args.length <= 1)
             {
-                player.sendMessage("§6[§bItemizer§6] §aYou did not a subcommand: add, clear");
+                player.sendMessage("§6[§bItemizer§6] §aYou did not a subcommand: add, set, remove, clear, help");
                 return true;
             }
 
+            if (args[1].equalsIgnoreCase("help"))
+            {
+                player.sendMessage("§c===§6[§bItemizer§6] §aLore Commands§c===");
+                player.sendMessage("");
+                player.sendMessage("§b/itemizer lore add <line> - §cAdds a line to your lore.");
+                player.sendMessage("");
+                player.sendMessage("§b/itemizer lore set <lore> - §cStarts off your item's list of lores.");
+                player.sendMessage("");
+                player.sendMessage("§b/itemizer lore clear - §cClears your lore(s).");
+                player.sendMessage("");
+                player.sendMessage("§b/itemizer lore remove <line> - §cClears a line of your lore.");
+                return true;
+            }
             if (args[1].equalsIgnoreCase("set"))
             {
                 String lore = StringUtils.join(ArrayUtils.subarray(args, 2, args.length), " ");
@@ -125,8 +168,72 @@ public class ItemizerCommand implements CommandExecutor {
                 player.sendMessage("§6[§bItemizer§6] §aYour item's lore has been set to '" + ChatColor.translateAlternateColorCodes('&', lore) + "'");
                 return true;
             }
+
+            if (args[1].equalsIgnoreCase("add"))
+            {
+                String lore = StringUtils.join(ArrayUtils.subarray(args, 2, args.length), " ");
+
+                assert itemMeta != null;
+
+                List<String> lores;
+
+                if (itemMeta.hasLore())
+                {
+                    lores = itemMeta.getLore();
+                } else {
+                    player.sendMessage("§6[§bItemizer§6] §aYou currently do not have any lores, to add one, please use /itemizer lore set <lore>, and then proceed.");
+                    return true;
+                }
+                if (args.length < 3)
+                {
+                    player.sendMessage("§6[§bItemizer§6] §aYou did not specify a lore to add.");
+                    return true;
+                }
+                assert lores != null;
+                lores.add(ChatColor.translateAlternateColorCodes('&', lore));
+                itemMeta.setLore(lores);
+                item.setItemMeta(itemMeta);
+                player.sendMessage("§6[§bItemizer§6] §aYou have added the following lore to your item: '" + ChatColor.translateAlternateColorCodes('&', lore) + "'");
+                return true;
+            }
+            if (args[1].equalsIgnoreCase("remove"))
+            {
+                assert itemMeta != null;
+
+                List<String> lores;
+
+                if (itemMeta.hasLore())
+                {
+                    lores = itemMeta.getLore();
+                } else {
+                    player.sendMessage("§6[§bItemizer§6] §aYou currently do not have any lores, to add one, please use /itemizer lore set <lore>, and then proceed.");
+                    return true;
+                }
+
+                if (args.length < 3) //itemizer0 lore1 remove2 clear3
+                {
+                    player.sendMessage("§6[§bItemizer§6] §aYou did not specify a line to remove.");
+                    return true;
+                }
+                int lineNumber = Integer.parseInt(args[2]);
+                try {
+                    assert lores != null;
+                    lores.remove(lineNumber);
+                } catch (Exception e)
+                {
+                    player.sendMessage("§6[§bItemizer§6] §aPlease remember that every lore's line starts with the number 0, not 1. Example:");
+                    player.sendMessage("§c1st Lore - §bLine 0");
+                    player.sendMessage("§c2nd Lore - §bLine 1");
+                    return true;
+                }
+                itemMeta.setLore(lores);
+                item.setItemMeta(itemMeta);
+                player.sendMessage("§6[§bItemizer§6] §aYou have successfully removed that line.");
+                return true;
+            }
             if (args[1].equalsIgnoreCase("clear"))
             {
+                assert itemMeta != null;
                 itemMeta.setLore(null);
                 item.setItemMeta(itemMeta);
                 player.sendMessage("§6[§bItemizer§6] §aYour item's lore has been cleared");
@@ -167,8 +274,10 @@ public class ItemizerCommand implements CommandExecutor {
             {
                 playerHead.setOwningPlayer(null);
             }
+            //noinspection deprecation
             playerHead.setOwningPlayer(Bukkit.getOfflinePlayer(playerName));
             item.setItemMeta(playerHead);
+            player.sendMessage("§6[§bItemizer§6] §aSuccessfully set player skull owner to: '§c" + playerName + "§a'");
             return true;
         }
         return false;
